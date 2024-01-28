3b10996da26dadf25156b051eb96b28dec4e7312
Nagarjuna-09 <nagarjunanathani434@gmail.com>
2024-01-27 21:11:17 -0500

    kafka readstream, serialiation, processing, and writeStream code included

--- src/pack/obj.scala
+++ src/pack/obj.scala
@@ -23,6 +23,26 @@ object obj {
     val spark = SparkSession.builder().getOrCreate( )
     import spark.implicits._
     
+    // reads the data into a fixed unbounded input table schema 
+    // schema contains key, value, topic, partition, offset, timestamp, timestampType
+    val df = spark.readStream
+              .format("kafka")
+              .option("kafka.bootstrap.servers","localhost:9092")
+              .option("subscribe","sstpk")
+              .load()
+              
+    // we know that data from kafka comes out in byte array serialized format
+    // By casting it as a string, we can read it
+    val df1 = df.withColumn("value", expr("cast(value as string)"))
+
+    //processing - adding a date stamp column to the data
+    val finaldf = df.withColumn("tdate", current_date)
+
+    df1.writeStream
+          .format("console") //use console if you are writing to console
+          .option("checkpointLocation","file:///F:/checkpoint")
+          .start() //use start if you want to write to a location
+          .awaitTermination()
 
   }
   }
